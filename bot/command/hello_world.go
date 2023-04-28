1792c3573220ff03d900665c09f3797ef9d26599
RestartFU <45609733+RestartFU@users.noreply.github.com>
2023-04-27 21:10:53 -0400

    various changes

--- bot/command/hello_world.go
+++ bot/command/hello_world.go
@@ -8,13 +8,9 @@ import (
 type HelloWorld struct{}
 
 func (HelloWorld) Run(interaction *cmd.Interaction) {
-	followup, err := interaction.DeferResponse()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	_, err = followup.Create(cmd.MessageResponse{
-		Content: "Hello, world!",
+	_, err := interaction.Respond(cmd.MessageResponse{
+		Content:   fmt.Sprintf("Hello, %s", interaction.Member().User.Username),
+		Ephemeral: true,
 	})
 	if err != nil {
 		fmt.Println(err)
